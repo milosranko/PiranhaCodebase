797dd1323169ed3b7a3d1fe05595bd03e5c8eb5c
Miloš Ranković <milos.ranko@live.com>
2023-01-29 19:06:50 +0100

    Implemented minifying and bundling of css and js files, adjusted gulp task runner

--- PiranhaCms.PublicWeb/gulpfile.js
+++ PiranhaCms.PublicWeb/gulpfile.js
@@ -1,14 +1,30 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    cssmin = require("gulp-cssmin"),
-    rename = require("gulp-rename");
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify');
 
-gulp.task('min:css', function () {
-    return gulp.src('assets/scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cssmin())
-        .pipe(rename({
-            suffix: ".min"
-        }))
+gulp.task('minify-css', () => {
+    return gulp.src('wwwroot/assets/css/style.css')
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest('wwwroot/assets/css'));
+});
+
+gulp.task('minify-js', () => {
+    return gulp.src('wwwroot/assets/js/main.js')
+        .pipe(minify({ ext: { min: '.min.js' } }))
+        .pipe(gulp.dest('wwwroot/assets/js'));
+});
+
+gulp.task('bundle-css', () => {
+    return gulp.src(['wwwroot/assets/css/bootstrap.min.css', 'wwwroot/lib/owlcarousel/assets/owl.carousel.min.css', 'wwwroot/assets/css/style.min.css'])
+        .pipe(concat('styles.bundle.css'))
+        .pipe(gulp.dest('wwwroot/assets/css'));
+});
+
+gulp.task('bundle-js', () => {
+    return gulp.src(['wwwroot/lib/jquery/jquery-3.6.3.min.js', 'wwwroot/lib/easing/easing.min.js', 'wwwroot/lib/waypoints/waypoints.min.js', 'wwwroot/lib/owlcarousel/owl.carousel.min.js', 'wwwroot/assets/js/main.min.js'])
+        .pipe(concat('scripts.bundle.js'))
+        .pipe(gulp.dest('wwwroot/assets/js'));
 });
