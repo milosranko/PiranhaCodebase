33d331060da398294fa8772b1ad217c39120cdd8
Miloš Ranković <milos.ranko@live.com>
2023-04-06 12:56:21 +0200

    Added test component

---
+++ PiranhaCms.PublicWeb/Components/Test.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PiranhaCMS.PublicWeb.Components
+{
+    public class Test : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = new List<string> { "Test", "Test 1" };
+            return View(items);
+        }
+    }
+}
