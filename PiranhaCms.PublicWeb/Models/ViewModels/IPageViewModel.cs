010b08ea209230ec89924a5350b0408e17f52776
Miloš Ranković <milos.ranko@live.com>
2024-05-11 10:46:54 +0200

    Small code optimization

--- PiranhaCms.PublicWeb/Models/ViewModels/IPageViewModel.cs
+++ PiranhaCms.PublicWeb/Models/ViewModels/IPageViewModel.cs
@@ -1,9 +1,11 @@
 using PiranhaCMS.ContentTypes.Pages.Base;
+using System.Text.Json.Serialization;
 
 namespace PiranhaCMS.PublicWeb.Models.ViewModels;
 
 public interface IPageViewModel<out T> where T : IPage
 {
+    [JsonIgnore]
     T CurrentPage { get; }
     HeaderViewModel Header { get; set; }
     FooterViewModel Footer { get; set; }
